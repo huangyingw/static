1782c94310a449b1046618cf25aa505f628748b8
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 22:08:56 +0800

    n

--- static.cpp
+++ static.cpp
@@ -2,23 +2,36 @@
 #include <iostream>
 using namespace std;
 
-class A
+class Myclass
 {
-	public:
-  	virtual void foo() { cout << "A::foo() is called" << endl;}
+public:
+	Myclass(int a,int b,int c);
+	void GetSum();
+private:
+	int a,b,c;
+	static int Sum;//声明静态数据成员
 };
+int Myclass::Sum=0;//定义并初始化静态数据成员
 
-class B: public A
+Myclass::Myclass(int a,int b,int c)
 {
-	public:
-    virtual void foo() { cout << "B::foo() is called" << endl;}
-};
+	this->a=a;
+	this->b=b;
+	this->c=c;
+	Sum+=a+b+c;
+}
 
+void Myclass::GetSum()
+{
+	cout<<"Sum="<<Sum<<endl;
+}
 
 int main(void)
 {
-	A * a = new B();
-	//B * b = new A();//error: invalid conversion from ‘A*’ to ‘B*’
-	a->foo();       // 在这里，a虽然是指向A的指针，但是被调用的函数(foo)却是B的!
-
+	Myclass M(1,2,3);
+	M.GetSum();
+	Myclass N(4,5,6);
+	N.GetSum();
+	M.GetSum();
+	return 0;
 }
